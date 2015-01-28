0c6720e28aef353f2b7a075960c00e6f376fcd62
Bill Waggoner <ctgreybeard@gmail.com>
2015-01-28 12:41:26 -0500

    Initial Commit

---
+++ TableViewSort/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TableViewSort
+//
+//  Created by William Waggoner on 1/28/15.
+//  Copyright (c) 2015 William C Waggoner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
